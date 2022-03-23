2d78a9dd819d39fdc19c692e10b8e5e29ac04515
Nikhil Desai <nikhild@prospectatech.local>
2022-03-23 11:58:18 +0530

    changes

---
+++ src/app/components/list-of-files/list-of-files.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { FileUploadService } from 'src/app/services/file-upload.service';
+
+@Component({
+  selector: 'app-list-of-files',
+  templateUrl: './list-of-files.component.html',
+  styleUrls: ['./list-of-files.component.css']
+})
+export class ListOfFilesComponent implements OnInit {
+  usr: any = [];
+  constructor(
+    private uploadService : FileUploadService
+  ) { }
+
+  ngOnInit(): void {
+
+    this.getFiles();
+  }
+
+  getFiles(){
+    this.uploadService.getFiles().then(res => {
+      this.usr=res.docs.map(e=> e.data())
+    })
+  }
+}
